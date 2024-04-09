e3942a173989d28bcb9f0a5156d77fbed4f43969
nazeemrizwi <150274609+nazeemrizwi@users.noreply.github.com>
2024-04-09 14:12:52 +0530

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -39,3 +39,18 @@ select product .pname,orders.orderid from product join orders on product.orderid
 | glass    |       7 |
 | pen      |       9 |
 +----------+---------+
+
+create table course(staffid int,cusno int,cusname varchar(25),primary key(staffid));
+insert into course values(101,991,"manu");
+insert into course values(102,221,"marya");
+insert into course values(103,333,"maya");
+insert into course values(104,343,"riya");
+insert into course values(105,443,"kiran");
+CREATE TABLE facuality (fno INT PRIMARY KEY,fname VARCHAR(25),staffid INT,FOREIGN KEY (staffid) REFERENCES course(staffid));
+insert into facuality values(201,"rony",101);
+insert into facuality values(202,"sunny",102);
+insert into facuality values(203,"saya",103);
+insert into facuality values(204,"saya",104); 
+insert into facuality values(205,"saya",105);
+SELECT staffid, cusname FROM course o WHERE EXISTS (SELECT 1 FROM facuality p WHERE p.staffid = o.staffid);
+
